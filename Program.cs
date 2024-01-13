4dba6f412d37d80d65d1a024401f7b9ddf9dd25b
Tyler <tfritzy@gmail.com>
2024-01-13 12:07:10 -0800

    Updating props

--- Program.cs
+++ Program.cs
@@ -1,9 +1,9 @@
 ﻿if (args.Length > 1 && args[1] == "server")
 {
     var matchmaker = new Matchmaker();
-    await matchmaker.Run("http://localhost:8080/");
+    await matchmaker.Run("http://20.118.203.253:64132/");
 }
 else
 {
-    await WebSocketClient.Connect("ws://localhost:8080/");
+    await WebSocketClient.Connect("ws://20.118.203.253:64132/");
 }

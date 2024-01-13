2c2750fea0832862165a5e1fceb5ebe2af621179
Tyler <tfritzy@gmail.com>
2024-01-13 09:38:53 -0800

    update to hole punch

--- Program.cs
+++ Program.cs
@@ -1,12 +1,2 @@
-﻿if (args.Length > 0 && args[0] == "client")
-{
-    Console.WriteLine("Starting client...");
-    var client = new Client();
-    client.Run();
-}
-else
-{
-    Console.WriteLine("Starting server...");
-    Server server = new Server();
-    server.Run();
-}
+﻿var server = new HolePunchServerTest();
+server.Run();

fb3046b2bda9faee2f06b778b4c21289593c39d0
Tyler <tfritzy@gmail.com>
2024-01-13 15:49:33 -0800

    Already built and configured

--- Program.cs
+++ Program.cs
@@ -1,8 +1,10 @@
 using UDP;
 
 UDPSocket s = new UDPSocket();
-s.Server("127.0.0.1", 27000);
+s.Server("127.0.0.1", 64132);
 
-UDPSocket c = new UDPSocket();
-c.Client("127.0.0.1", 27000);
-c.Send("TEST!");
+// UDPSocket c = new UDPSocket();
+// c.Client("20.29.48.111", 64132);
+// c.Send("TEST!");
+
+Console.ReadKey();
